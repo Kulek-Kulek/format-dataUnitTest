7c89c17cdabbe37611f7e8d9fd03950d5bd4a5ad
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-04-06 21:22:39 +0200

    Add

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const formatDate = (timeInSeconds) => {
-  return `${timeInSeconds}s`;
+function formatDate () {
+  const os = require('os');
+
+
+let uptime = (os.uptime());
+let hour = `${uptime}` / 3600;
+let minute = (`${hour}` - Math.floor(hour)) * 60;
+let second = (`${minute}` - Math.floor(minute)) * 60;
+
+if (`${uptime}` < 60) {
+	console.log(Math.floor(`${second}`) + `s`)
+} if (`${uptime}` >= 60 && `${uptime}` < 3600) {
+	console.log(Math.floor(`${minute}`) + `m`, Math.floor(`${second}`) + `s`);
+} if (`${uptime}` >= 3600) {
+	console.log(`Current uptime is: ` + Math.floor(`${hour}`) + `h`, Math.floor(`${minute}`) + `m`, Math.floor(`${second}`) + `s`);
 }
+}
+
+
 
 module.exports = formatDate;
--- uptime.js
+++ uptime.js
@@ -1,6 +1,4 @@
 const os = require('os');
 const formatDate = require('./index');
 
-const uptime = formatDate(os.uptime());
-
-console.log(`Current uptime is ${uptime}`);
+const totaltime = formatDate(os.uptime());
